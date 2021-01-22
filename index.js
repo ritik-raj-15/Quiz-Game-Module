c7caf878f40fec6b983a3ace40b9a0ad7bebe325
Ritik Raj <ritik1384.cse18@chitkara.edu.in>
2021-01-23 01:03:22 +0530

    first commit (Made Database of Quiz)

---
+++ index.js
@@ -0,0 +1,101 @@
+var questions=[{
+ ques:"A complete graph can have",
+ ops:{
+     a:"N^2 Spanning Trees",
+     b:"N^(N-2) Spanning Trees",
+     c:"N^(N+1) Spanning Trees",
+     d:"N^N Spanning Trees"
+ },
+ correct:"N^(N-2) Spanning Trees"
+},
+{
+ ques:"What is full form of CSS",
+ ops:{
+    a:"Color Style Sheets",
+    b:"Cascade Sheets Style",
+    c:"Cascade Style Sheet",
+    d:"Cascading Style Sheets"
+},
+ correct:"Cascading Style Sheets"
+},
+{
+ ques:"How to include JavaScript in HTML document",
+ ops:{
+    a:"From javascript:encoding",
+    b:"External file specified by the src attribute of a “script” tag",
+    c:"By using a header tag",
+    d:"By using body tag"
+},
+ correct:"External file specified by the src attribute of a “script” tag"
+},
+{
+ ques:"How to declare variable in JavaScript",
+ ops:{
+    a:"var",
+    b:"obj",
+    c:"jvar",
+    d:"None of these"
+},
+ correct:"var"
+},
+{
+ ques:"What => means in JavaScript",
+ ops:{
+    a:"Immediately Invoked Function",
+    b:"Named Function",
+    c:"Arrow Function",
+    d:"Anonymous Function"
+},
+ correct:"Arrow Function"
+},
+{
+ ques:"Which of these is not a JavaScript framework",
+ ops:{
+    a:"Rico",
+    b:"Meteor",
+    c:"jQuery",
+    d:"Django"
+},
+ correct:"Arrow Function"
+},
+{
+ ques:"How do you define objects in JS",
+ ops:{
+    a:"[]",
+    b:"{}",
+    c:"()",
+    d:"{()}"
+},
+ correct:"{}"
+},
+{
+ ques:"Which of these is not a valid JS version",
+ ops:{
+    a:"EX19",
+    b:"ES1",
+    c:"ES6",
+    d:"ECMAScript 2018"
+},
+ correct:"EX19"
+},
+{
+ ques:"Whats the output of '2' + 2",
+ ops:{
+    a:"22",
+    b:"4",
+    c:"both a and b",
+    d:"None of these"
+},
+ correct:"22"
+},
+{
+ ques:"How to check if a value is NaN in javaScript",
+ ops:{
+    a:"nonNaN()",
+    b:"NaN()",
+    c:"isNaN(val)",//
+    d:"None of the above"
+},
+ correct:"isNaN(val)"
+}
+];
